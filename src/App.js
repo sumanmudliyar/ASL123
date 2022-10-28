d376a10e464df77dafb4317ab6c83f76a92999b8
sumanmudliyar35 <sumanmudliyar35@gmail.com>
2022-10-28 10:21:04 +0530

    uploaded

---
+++ src/App.js
@@ -0,0 +1,316 @@
+
+import airbnb from './airbnb.png';
+import globe from './globe.png';
+import s1 from './s1.png';
+import s2 from './s2.png';
+import s3 from './s3.png';
+import s5 from './s5.png';
+import s6 from './s6.png';
+import s7 from './s7.png';
+import s8 from './s8.png';
+import s9 from './s9.png';
+import s10 from './s10.png';
+import ss2 from './ss2.png';
+import ss3 from './ss3.png';
+import ss4 from './ss4.png';
+import ss5 from './ss5.png';
+import ss6 from './ss6.png';
+import ss7 from './ss7.png';
+import {Card} from "react-bootstrap";
+import sss2 from './sss2.png';
+import a1 from './a1.png';
+
+
+
+
+
+
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        
+        <span><img src={airbnb} class="titleimg"></img></span>
+        <span><p class="titlename"><strong>airbnb</strong></p></span>
+        
+        
+       
+      
+        
+      </header>
+      <div class="b1">
+      <button><b>Become a host</b></button>
+      <img src={globe}></img>
+      </div>
+      <div class="aa1">
+      <div class="a1"><img src={a1}></img></div>
+      </div>
+        <br></br>
+      
+      <div class="line"></div>
+
+      <div class="new">
+        <h2><b>New this week</b></h2>
+      </div>
+      <br></br>
+
+
+      <div class="scrolling">
+       
+        <img src={s1} class="sss1"></img>&nbsp;&nbsp;&nbsp;&nbsp;
+        
+
+
+        <img src={s2} class="sss2"></img>&nbsp;&nbsp;&nbsp;&nbsp;
+
+
+        <img src={s3} class="sss3"></img>
+      </div><br></br>
+      
+  
+      
+      <div class="buttons">
+        <div class="div1">
+        <button>Dates</button>&nbsp;&nbsp;&nbsp;
+  
+        <button>Group Size</button>&nbsp;&nbsp;&nbsp;
+        
+        <button>More filters</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="vl"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+        <div class="div22">
+        <div class="div2">
+        <button>Great for groups</button>&nbsp;&nbsp;&nbsp;
+        <button>Family-friendly</button>&nbsp;&nbsp;&nbsp;
+        <button>Animals</button>&nbsp;&nbsp;&nbsp;
+        <button>Arts and Writing</button>&nbsp;&nbsp;&nbsp;
+        <button>Baking</button>&nbsp;&nbsp;&nbsp;
+        <button>Cooking</button>&nbsp;&nbsp;&nbsp;
+        <button>Dance</button>&nbsp;&nbsp;&nbsp;
+        <button>Drink</button>&nbsp;&nbsp;&nbsp;
+        <button>Entertainment</button>&nbsp;&nbsp;&nbsp;
+        <button>Dance</button>&nbsp;&nbsp;&nbsp;
+        <button>Drink</button>&nbsp;&nbsp;&nbsp;
+        <button>Entertainment</button>
+        
+        </div>
+        </div>
+
+      </div>
+<br>
+</br>
+<br>
+</br>
+<div class="plan"> 
+  <h4><b>Plan a trip with help from local hosts around the world</b></h4>
+</div>
+
+      <div class="img12">
+      <div class="s5">
+
+<Card style={{ width: '13rem' }}>
+<Card.Img orientation="top" src={s5} />
+<Card.Body style={{ height:'130px'}}>
+<Card.Title>United States</Card.Title>
+<Card.Text >
+<p>
+  Plan The Perfect New York <br></br> Vacation<br></br>
+<b> From 1,562</b>/person</p>
+
+</Card.Text>
+
+</Card.Body>
+</Card>
+</div>
+<div class="s6">
+
+      <Card style={{ width: '13rem' }}>
+  <Card.Img orientation="top" src={s6} />
+  <Card.Body style={{ height:'130px'}}>
+    <Card.Title>Spain</Card.Title>
+    <Card.Text >
+      <p>
+        Design your trip to Barcelona<br></br> with Gemma<br></br>
+      <b> From 3,131</b>/person</p>
+      
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+</div>
+<div class="s7">
+<Card style={{ width: '13rem' }}>
+  <Card.Img orientation="top" src={s8} />
+  <Card.Body style={{ height:'130px'}}>
+    <Card.Title>South Korea</Card.Title>
+    <Card.Text >
+      <p>
+        Cast a custom trip to Korea<br></br> with jay<br></br>
+      <b> From 3,475</b>/person</p>
+      
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+
+      
+</div>
+<div class="s8">
+
+      <Card style={{ width: '13rem' }}>
+  <Card.Img orientation="top" src={s8} />
+  <Card.Body style={{ height:'130px'}}>
+    <Card.Title>Mexico</Card.Title>
+    <Card.Text >
+      <p>
+        Live Tulum like a local<br></br>
+      <b> From 620</b>/person</p>
+      
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+</div>
+<div class="s9">
+
+      <Card style={{ width: '13rem' }}>
+  <Card.Img orientation="top" src={s9} />
+  <Card.Body style={{ height:'130px'}}>
+    <Card.Title>United States</Card.Title>
+    <Card.Text >
+      <p>
+        Discover Sunny Fort<br></br> Lauderdale<br></br>
+      <b> From 1,726</b>/person</p>
+      
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+</div>
+<div class="s10">
+
+      <Card style={{ width: '13rem' }}>
+  <Card.Img orientation="top" src={s10} />
+  <Card.Body style={{ height:'130px'}}>
+    <Card.Title>United States</Card.Title>
+    <Card.Text >
+      <p>
+        Plan  a trip to amazing<br></br> Austin<br></br>
+      <b> From 2,055</b>/person</p>
+      
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+</div>
+
+        <div class="ss2">
+
+      <Card style={{ width: '13rem' }}>
+  <Card.Img orientation="top" src={ss2} />
+  <Card.Body style={{ height:'130px'}}>
+    <Card.Title>Italy</Card.Title>
+    <Card.Text >
+      <p>
+        Pasta from Grandmas<br></br>
+      <b> From 2,719</b>/person</p>
+      
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+</div>
+<div class="ss3">
+
+<Card style={{ width: '13rem' }}>
+<Card.Img orientation="top" src={ss3} />
+<Card.Body style={{ height:'130px'}}>
+<Card.Title>Poland</Card.Title>
+<Card.Text >
+<p>
+  Crack the mystery Escape<br></br>Room Game<br></br>
+<b> From 1,299</b>/person</p>
+
+</Card.Text>
+
+</Card.Body>
+</Card>
+</div>
+<div class="ss4">
+
+<Card style={{ width: '13rem' }}>
+<Card.Img orientation="top" src={ss4} />
+<Card.Body style={{ height:'130px'}}>
+<Card.Title>Spain</Card.Title>
+<Card.Text >
+<p>
+  'No Spain no Game' The <br></br>Fabulous Game<br></br>
+<b> From 1,319</b>/person</p>
+
+</Card.Text>
+
+</Card.Body>
+</Card>
+</div>
+<div class="ss5">
+
+<Card style={{ width: '13rem' }}>
+<Card.Img orientation="top" src={ss5} />
+<Card.Body style={{ height:'130px'}}>
+<Card.Title>France</Card.Title>
+<Card.Text >
+<p>
+  'From Paris with Laughs' The <br></br>Scavenger Hunt<br></br>
+<b> From 1,154</b>/person</p>
+
+</Card.Text>
+
+</Card.Body>
+</Card>
+</div>
+<div class="ss6">
+
+<Card style={{ width: '13rem' }}>
+<Card.Img orientation="top" src={ss6} />
+<Card.Body style={{ height:'130px'}}>
+<Card.Title>Czech Republic</Card.Title>
+<Card.Text >
+<p>
+  The Plague Doctor of <br></br>Prague-Halloween Edition<br></br>
+<b> From 1,345</b>/person</p>
+
+</Card.Text>
+
+</Card.Body>
+</Card>
+</div>
+<div class="ss7">
+
+<Card style={{ width: '13rem' }}>
+<Card.Img orientation="top" src={ss7} />
+<Card.Body style={{ height:'130px'}}>
+<Card.Title>Mexico</Card.Title>
+<Card.Text >
+<p>
+  Make Mexican Street Tacos<br></br> with a Pro Chef<br></br>
+<b> From 2,067</b>/person</p>
+
+</Card.Text>
+
+</Card.Body>
+</Card>
+</div>
+
+      
+        
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
